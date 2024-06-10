7078786cbc3b30418d666b2c4488f311dd2a1a52
José Miguel González Guzmán <107879363+JosemAVSG@users.noreply.github.com>
2024-06-10 01:51:53 -0500

    adding routes

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+
+    @Get('/products')
+    getProducts() {
+        return 'This action returns all products';
+    }
+}
